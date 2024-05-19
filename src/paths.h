9f5d6e15b68c6a32f2e1492385f08ca50aa69934
h-rotkiewicz <116381464+h-rotkiewicz@users.noreply.github.com>
2024-05-19 18:14:27 +0200

    Update paths.h

--- src/paths.h
+++ src/paths.h
@@ -15,10 +15,8 @@ struct Paths {
     static const fs::path shaders_vs;
     static const fs::path shaders_fs;
     static const fs::path resources_metalic;  
-    static const fs::path resources_awesomeface;
 };
 
 inline const fs::path Paths::shaders_vs = fs::absolute(std::string(SHADERS_DIR) + "/transform.vs");
 inline const fs::path Paths::shaders_fs = fs::absolute(std::string(SHADERS_DIR) + "/transform.fs");
-inline const fs::path Paths::resources_awesomeface = fs::absolute(std::string(RESOURCES_DIR)  + "/awesomeface.png");
 inline const fs::path Paths::resources_metalic = fs::absolute(std::string(RESOURCES_DIR) + "/metal-texture-25.jpg");
